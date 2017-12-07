5eda473fa54ed5b93622c8adc842bca4caf8b474
kachalovsky <andrei7a@mail.ru>
2017-12-07 23:40:58 +0300

    repaired labs with gitignore

---
+++ xUnit-Webdriver/xUnit-Webdriver/Class1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnit_Webdriver
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void MethodOne()
+        {
+            IWebDriver webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            string url = "https://github.com";
+            webDriver.Navigate().GoToUrl(url);
+            webDriver.FindElement(By.LinkText("Sign in")).Click();
+            webDriver.FindElement(By.Id("login_field")).SendKeys("testautomationuser");
+            webDriver.FindElement(By.Id("password")).SendKeys("Time4Death!");
+            webDriver.FindElement(By.Name("commit")).Click();
+        }
+    }
+}
---
+++ xUnit-Webdriver/xUnit-Webdriver/Kachel.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnit_Webdriver
+{
+    public class Kachel  
+    {
+        public Kachel()
+        {
+
+        }
+    }
+}
